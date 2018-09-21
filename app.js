b665a6bbfbc523a5b36590dd96e88a4f76b5dba4
Vawi <val.winnen@gmail.com>
2018-09-21 16:16:00 +0200

    Modification architecture, connection mongodb

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
+var createError = require('http-errors');
 var express = require('express');
-var session = require('cookie-session'); 
-var bodyParser = require('body-parser'); 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var pg = require("pg");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-const mountRoutes = require('./routes')
 
-mountRoutes(app)
 
-app.listen(4000, function () {
-    console.log('Server is running on port 4000');
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://vawi:vawi93@ds211613.mlab.com:11613/local_biblio';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/users/cool', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
